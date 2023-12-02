a521249cfeab981eb5be098e2904b6b5ed5aabea
Aaron Ripley <ripleya1@xavier.edu>
2023-12-02 16:59:19 -0500

    just a guess

--- shuttle.c
+++ shuttle.c
@@ -11,11 +11,12 @@ Constraints:
 Hints:
 - Think about the state you want to keep and the places where you need a semaphore:
 - It's likely you'll want to keep track of the attendees, so you'll want an integer and a mutex to do that
-- You'll probably also need another semaphore so they actually be waiting when the shuttle is not there.
-- One way to think about the shuttle is that it is a semaphore initialized to 30. Getting on is a wait and getting off is a post.
+    - You'll probably also need another semaphore so they actually be waiting when the shuttle is not there.
+- One way to think about the shuttle is that it is a semaphore initialized to 30. 
+    - Getting on is a wait and getting off is a post.
 - How does the shuttle know to go? (This is a little like the Old Woman, it needs to wait for a post from someone.)
 - The shuttle gets to the venue and has to dump the passengers. How is that managed?
-- For ease of implementation, assume for the moment that the shuttle only picks up attendees at one end of the route, and then heads back.
+- For ease of implementation, assume the shuttle only picks up attendees at one end of the route, and then heads back.
 */
 
 #include <stdio.h>
@@ -29,13 +30,14 @@ Hints:
 #include "zemaphore.h" 
 
 Zem_t *sh;
-Zem_t checkAttendees;
+Zem_t *checkAttendees;
+Zem_t *busStop;
 int attendees = 0;
 
 /*
 need some sort of boolean indicating that the number of attendees is at 0 so the shuttle can leave
 maybe that being true can trigger a post which triggers the shuttle leaving
-remember that the shuttle also needs to leave if it's full
+but remember that the shuttle also needs to leave if it's full
 so shuttle_is_full or attendees_at_0 whichever comes first should tell the shuttle to leave
 
 need a "waiting queue" of some kind for attendees so they don't board if they arrived while the shuttle was boarding
@@ -46,21 +48,45 @@ int main(int argc, char *argv[]){
     sh = malloc(sizeof(Zem_t));
     Zem_init(&sh, 30);
 
+    // make sure that we can check the attendees variable
+    checkAttendees = malloc(sizeof(Zem_t));
+    Zem_init(&checkAttendees, 1);
+
+    // represents the people at the bus stop
+    busStop = malloc(sizeof(Zem_t));
+    Zem_init(&busStop, 0);
+
     pthread_t s;
     Pthread_create(&s, NULL, shutt, NULL);
 
     printf("Shuttling started\n");
 }
 
-// kind of like child from littleOldWoman??
 void *shutt(void *arg){
+    for(;;){
+        fillBus();
+        
+    }
+}
 
+void fillBus(){
+    int numOnBus = 0;
+    while(numOnBus < 30 && ){
+        numOnBus++;
+    }
 }
 
 void getOn(){
+    Zem_post(&busStop);
     Zem_wait(&sh);
 }
 
 void getOff(){
+    Zem_wait(&busStop);
     Zem_post(&sh);
+}
+
+void travel(){
+    usleep(10);
+    return;
 }
